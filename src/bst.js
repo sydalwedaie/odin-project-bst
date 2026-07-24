b012e93184e3a7696a1c5826b38d8e17fd30ac07
Sayed Alwedaie <5216479+sydalwedaie@users.noreply.github.com>
2026-07-24 18:35:31 +0300

    Implement tests for creating trees.

--- src/bst.js
+++ src/bst.js
@@ -8,7 +8,7 @@ class Node {
   }
 }
 
-class Tree {
+export class Tree {
   #sanitizedArray;
   constructor(array) {
     this.#sanitizedArray = this.#sanitizeArray(array);
@@ -70,26 +70,26 @@ class Tree {
   }
 }
 
-console.clear();
+// console.clear();
 // Test initialization
-const input = [7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
-const myTree = new Tree(input);
+// const input = [7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const myTree = new Tree();
 prettyPrint(myTree.root);
-// console.dir(myTree.root, { depth: null, colors: true });
+console.dir(myTree.root, { depth: null, colors: true });
 
 // Test includes
-console.log("Test includes: ", "should fail -1 and 0");
-const includesTestItems = [...input, -1, 0];
-includesTestItems.forEach((item) => {
-  console.assert(myTree.includes(item), `${item} is not in the tree`);
-});
+// console.log("Test includes: ", "should fail -1 and 0");
+// const includesTestItems = [...input, -1, 0];
+// includesTestItems.forEach((item) => {
+//   console.assert(myTree.includes(item), `${item} is not in the tree`);
+// });
 
 // Test insert
-console.log("Test insert: ");
-const anotherTree = new Tree([1, 2]);
-anotherTree.insert(0);
-anotherTree.insert(12);
-anotherTree.insert(22);
-anotherTree.insert(5);
-prettyPrint(anotherTree.root);
-console.dir(anotherTree.root, { depth: null, colors: true });
+// console.log("Test insert: ");
+// const anotherTree = new Tree([1, 2]);
+// anotherTree.insert(0);
+// anotherTree.insert(12);
+// anotherTree.insert(22);
+// anotherTree.insert(5);
+// prettyPrint(anotherTree.root);
+// console.dir(anotherTree.root, { depth: null, colors: true });
---
+++ tests/bst.test.js
@@ -0,0 +1,44 @@
+import { describe, expect, test } from "@jest/globals";
+import { Tree } from "../src/bst.js";
+
+describe("Test building trees", () => {
+  test("should convert an array to tree", () => {
+    const input = [7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+    const myTree = new Tree(input);
+    const expected = {
+      data: 8,
+      left: {
+        data: 4,
+        left: { data: 3, left: null, right: null },
+        right: {
+          data: 5,
+          left: null,
+          right: { data: 7, left: null, right: null },
+        },
+      },
+      right: {
+        data: 67,
+        left: {
+          data: 9,
+          left: null,
+          right: { data: 23, left: null, right: null },
+        },
+        right: {
+          data: 324,
+          left: null,
+          right: { data: 6345, left: null, right: null },
+        },
+      },
+    };
+
+    expect(myTree.root).toEqual(expected);
+  });
+
+  test("should return null for empty array or no input", () => {
+    const treeEmptyArray = new Tree([]);
+    const treeNoInput = new Tree();
+    const expected = null;
+    expect(treeEmptyArray.root).toEqual(expected);
+    expect(treeNoInput.root).toEqual(expected);
+  });
+});
